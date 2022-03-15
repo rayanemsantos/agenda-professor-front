2252f5cddadf65d474f412919eea6ac1dcf1b9e8
Ariel <37940722+arielcavalcante@users.noreply.github.com>
2022-03-14 22:57:55 -0300

    WIP-Cria calendario e modal, adiciona estilos

--- src/pages/Calendario/Calendario.js
+++ src/pages/Calendario/Calendario.js
@@ -1,9 +1,10 @@
-import React, {useEffect, useState} from "react";
-import {
-    Redirect,
-    Link
-} from "react-router-dom";
-import moment from "moment";
+import React, { useEffect, useState } from 'react';
+import { Redirect, Link } from 'react-router-dom';
+import moment from 'moment';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction';
+import { Box, Stack, Typography } from '@mui/material';
 
 import TabsComponent from '../../components/TabsComponent';
 import HeaderComponent from '../../components/HeaderComponent';
@@ -11,61 +12,97 @@ import NewCalendario from './NewCalendario';
 
 import * as services from '../../services/service';
 
+function Calendario({ history }) {
+	const [events, setEvents] = useState([]);
+	const [logged, setLogged] = useState(true);
+	const [user, setUser] = useState(null);
 
-function Calendario({history}) {
-    const [events, setEvents] = useState([]);
-    const [logged, setLogged] = useState(true);
-    const [user, setUser] = useState(null);
+	const [open, setOpen] = React.useState(false);
+	const handleOpen = () => setOpen(true);
+	const handleClose = () => setOpen(false);
 
-    
-    const getUser = () => {
-        if(!localStorage.getItem("user")){
-            setLogged(false)
-        } else {
-            var data = JSON.parse(localStorage.getItem("user"))
-            setUser(data)
-            getEvents()
-        }
-    }
-    const getEvents = () => {
-        services.fetchEvents().then((res) => {
-            setEvents(res.data)
-        }).catch((err) => console.log(err))
-    }
-    useEffect(() => {
-        getUser()
-    }, []);
+	// RAY: Tipo de usuário mocado
+	const usuario = 'secretaria';
 
-  if (!logged) {
-    return <Redirect to="/login" />;
-  }
-  return (
-      user &&
-      <>
-        <HeaderComponent user={user} history={history} handleClick={(page) => history.push(page)}/>
-        <main>
-        <NewCalendario onCreateEvent={() => getEvents()}/>
-        <section>
-            <h2>Calendário</h2>
-            <p className="subtitle hora">{moment().format('DD/MM/YY')}</p>
-            <div className="task-content">
-            {events.map((item) => {
-                return <div className="atividade">
-                    <ul>
-                        <li>
-                            <span className="serie">{item.evento}</span>
-                        </li>
-                        <li>
-                            <span className="serie">{item.data}</span>
-                        </li>                                                                
-                    </ul>
-                </div>                        
-            })}         
-            </div>                 
-        </section>        
-        </main>
-    </>
-  );
+	const getUser = () => {
+		if (!localStorage.getItem('user')) {
+			setLogged(false);
+		} else {
+			var data = JSON.parse(localStorage.getItem('user'));
+			setUser(data);
+			getEvents();
+		}
+	};
+
+	const getEvents = () => {
+		services
+			.fetchEvents()
+			.then(res => {
+				setEvents(res.data);
+			})
+			.catch(err => console.log(err));
+	};
+	useEffect(() => {
+		getUser();
+	}, []);
+
+	if (!logged) {
+		return <Redirect to='/login' />;
+	}
+
+	return (
+		user && (
+			<>
+				<HeaderComponent
+					user={user}
+					history={history}
+					handleClick={page => history.push(page)}
+				/>
+				<Box type='main'>
+					<Box type='section'>
+						<Box className='content-header'>
+							<Typography variant='h4' sx={{ mt: 3 }}>
+								Calendário
+							</Typography>
+							<Typography variant='overline' className='subtitle hora'>
+								{moment().format('dddd, DD MMM, LT')}
+							</Typography>
+						</Box>
+
+						{/* <NewCalendario onCreateEvent={() => getEvents()} /> */}
+
+						<Box className='task-content'>
+							<NewCalendario
+								open={open}
+								close={handleClose}
+								usuario={usuario}
+							/>
+							<FullCalendar
+								plugins={[dayGridPlugin, interactionPlugin]}
+								dateClick={handleOpen}
+								initialView='dayGridMonth'
+							/>
+							{/* {events.map(item => {
+								return (
+									<div className='atividade'>
+										<ul>
+											<li>
+												<span className='serie'>{item.evento}</span>
+											</li>
+											<li>
+												<span className='serie'>{item.data}</span>
+											</li>
+										</ul>
+									</div>
+								);
+							})} */}
+						</Box>
+					</Box>
+				</Box>
+				<TabsComponent />
+			</>
+		)
+	);
 }
 
 export default Calendario;
--- src/pages/Calendario/NewCalendario.js
+++ src/pages/Calendario/NewCalendario.js
@@ -1,85 +1,186 @@
-import React, { useEffect, useState } from 'react';
-import { Redirect, Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import Draggable from 'react-draggable';
+import {
+	Box,
+	Button,
+	Chip,
+	Dialog,
+	DialogActions,
+	DialogContent,
+	DialogTitle,
+	MenuItem,
+	Paper,
+	Stack,
+	Tab,
+	Tabs,
+	TextField,
+	Typography,
+} from '@mui/material';
+import { LocalizationProvider, TimePicker } from '@mui/lab';
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import moment from 'moment';
+import { DragHandleRounded, CloseRounded } from '@mui/icons-material';
+import { v4 as uuid } from 'uuid';
 
-import * as services from '../../services/service';
+// import * as services from '../../services/service';
 
 const defaultState = { evento: '', data: '', errorMessage: '' };
 
-function NewCalendario({ onCreateEvent }) {
-	const [logged, setLogged] = useState(true);
-	const [user, setUser] = useState(null);
-	const [form, setForm] = useState(defaultState);
-
-	const getUser = () => {
-		if (!localStorage.getItem('user')) {
-			setLogged(false);
-		} else {
-			var data = JSON.parse(localStorage.getItem('user'));
-			setUser(data);
-		}
-	};
-	const newEvent = () => {
-		services
-			.newCalendarItem(form)
-			.then(res => {
-				setForm(defaultState);
-				onCreateEvent();
-			})
-			.catch(err => console.log(err));
-	};
-	useEffect(() => {
-		getUser();
-	}, []);
-
-	if (!logged) {
-		return <Redirect to='/login' />;
-	}
+function Drag(props) {
 	return (
-		user && (
-			<>
-				<section className='form'>
-					<div className='atividade-form'>
-						<h2>Novo Evento</h2>
-						{/* {errorMessage !== '' && message(errorMessage)} */}
-						<form>
-							<div className='input wrapper'>
-								<input
-									type='text'
-									name='evento'
-									id='cadastro-evento'
-									value={form.evento}
-									onChange={e => setForm({ ...form, evento: e.target.value })}
-									required
-								/>
-								<label htmlFor='cadastro-evento'>Título</label>
-							</div>
-							<div className='input wrapper'>
-								<input
-									type='text'
-									name='data'
-									id='cadastro-data'
-									value={form.data}
-									onChange={e => setForm({ ...form, data: e.target.value })}
-									required
-								/>
-								<label htmlFor='cadastro-data'>Data</label>
-							</div>
-						</form>
+		<Draggable
+			handle='#draggable-dialog-title'
+			cancel={'[class*="MuiDialogContent-root"]'}
+		>
+			<Paper sx={{ width: '70vw' }} {...props} />
+		</Draggable>
+	);
+}
 
-						<button
-							className={'primary-button'}
-							style={{
-								opacity: form.evento === '' || form.data === '' ? 0.5 : 1,
-							}}
-							disabled={form.evento === '' || form.data === ''}
-							onClick={() => newEvent()}
-						>
-							Adicionar
-						</button>
-					</div>
-				</section>
-			</>
-		)
+const arr = ['Diário', 'Semanal', 'Quinzenal', 'Mensal'];
+function NewCalendario({ onCreateEvent, open, close, usuario }) {
+	// RAY: Não tenho ctz de como adicionar mais de uma aula no mesmo dia, mas ta aí
+	const [initialTime, setInitialTime] = useState(null);
+	const [endTime, setEndTime] = useState(null);
+	const [value, setValue] = useState(0);
+	const [repetition, setRepetition] = useState(arr[1]);
+	const [color, setColor] = useState('default');
+
+	const handleColor = e => {
+		color !== 'primary' ? setColor('primary') : setColor('default');
+	};
+
+	const handleChange = (event, newValue) => {
+		setValue(newValue);
+	};
+	const handleRepetition = e => {
+		setRepetition(e.target.value);
+	};
+
+	const weekDays = [
+		'Segunda',
+		'Terça',
+		'Quarta',
+		'Quinta',
+		'Sexta',
+		'Sábado',
+		'Domingo',
+	];
+
+	// RAY: O chip qd clica ta marcando tudo. Help.
+	// RAY: No momento o botão de salvar só ta fechando o modal
+
+	return (
+		<Dialog open={open} onClose={close} PaperComponent={Drag} usuario={usuario}>
+			<DialogTitle
+				sx={{
+					display: 'flex',
+					justifyContent: 'space-between',
+					cursor: 'move',
+				}}
+				aria-labelledby='draggable-dialog-title'
+				id='draggable-dialog-title'
+			>
+				<DragHandleRounded />
+				<CloseRounded onClick={close} sx={{ cursor: 'pointer' }} />
+			</DialogTitle>
+			<DialogContent sx={{ display: 'flex', flexDirection: 'column', gap: 1 }}>
+				<TextField
+					placeholder='Adicionar título'
+					fullWidth
+					variant='standard'
+					margin='normal'
+				/>
+				{usuario === 'secretaria' ? (
+					<Tabs value={value} onChange={handleChange}>
+						<Tab label='Aula' index={0} variant='text' />
+						<Tab label='Prova' index={1} variant='text' />
+						<Tab label='Evento' index={2} variant='text' />
+						<Tab label='Entrega de Atividade' index={3} />
+					</Tabs>
+				) : (
+					<Tabs value={value} onChange={handleChange}>
+						<Tab label='Prova' index={0} />
+						<Tab label='Evento' index={1} />
+						<Tab label='Entrega de Atividade' index={2} />
+					</Tabs>
+				)}
+				{usuario === 'secretaria' ? (
+					<Stack my={2} sx={{ gap: 0.5 }}>
+						<Typography variant='body2' fontWeight='medium'>
+							Repetir:
+						</Typography>
+						<Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+							<Box sx={{ display: 'flex', gap: 1 }}>
+								{weekDays.map(item => {
+									return (
+										<Chip
+											label={item.charAt(0)}
+											value={item}
+											onClick={handleColor}
+											color={color}
+										/>
+									);
+								})}
+							</Box>
+							<TextField
+								onChange={handleRepetition}
+								select
+								value={repetition}
+								variant='outlined'
+								sx={{ minWidth: '115px' }}
+								label='Frequência'
+							>
+								{arr.map(item => {
+									return (
+										<MenuItem value={item} key={uuid()}>
+											{item}
+										</MenuItem>
+									);
+								})}
+							</TextField>
+						</Box>
+					</Stack>
+				) : (
+					''
+				)}
+				<Stack>
+					<Typography variant='h6'>Seg</Typography>
+					<LocalizationProvider dateAdapter={AdapterDateFns}>
+						<Box sx={{ display: 'flex', gap: 2 }}>
+							<TimePicker
+								value={initialTime}
+								onChange={newTime => {
+									setInitialTime(newTime);
+								}}
+								inputFormat='hh:mm'
+								renderInput={params => (
+									<TextField variant='outlined' {...params} />
+								)}
+								required
+							/>
+							<TimePicker
+								value={endTime}
+								onChange={newTime => {
+									setEndTime(newTime);
+								}}
+								inputFormat='hh:mm'
+								renderInput={params => (
+									<TextField variant='outlined' {...params} />
+								)}
+								required
+							/>
+						</Box>
+					</LocalizationProvider>
+				</Stack>
+			</DialogContent>
+			<DialogActions>
+				<Button variant='text' onClick={close}>
+					Cancelar
+				</Button>
+				<Button onClick={close}>Salvar</Button>
+			</DialogActions>
+		</Dialog>
 	);
 }
 
